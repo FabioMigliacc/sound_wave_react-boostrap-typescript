1b4760331a0ed2b95d6cc62f8603dfdeaf0f3a97
Fabio Migliaccio <febbio9@gmail.com>
2023-02-08 13:17:39 +0100

    join page content created

--- src/routes/Discover.tsx
+++ src/routes/Discover.tsx
@@ -1,19 +1,46 @@
 import React from 'react'
 import Menu from '../components/menu'
 import Footer from '../components/footer'
+import { Col, Container, Row } from 'react-bootstrap'
+import more from '../assets/more.svg'
+import albums from '../assets/albums.svg'
+import microphone from '../assets/microphone.svg'
+import covers from '../assets/covers.jpg'
+
+
 
 export const Discover = () => {
-    return (<>
-        <>
-            <header>
-                <Menu />
-            </header>
-        </>
+    return (
 
-            <h1> dicover va</h1>
-        
-        < Footer/>
-        </>
+        <section>
+            <Container>
+                <header>
+                    <Menu />
+                </header>
+                <Row>
+                    <Col className='text-white'>
+                        <h1> Discover new music </h1>
+                        <div><img src={microphone} alt='' />
+                            <p>Charts</p></div>
+                        <div><img src={albums} alt='' />
+                            <p>Albums</p></div>
+                        <div><img src={more} alt='' />
+                            <p>More</p></div>
+
+
+                    </Col>
+                    <Col>
+                        <img src={covers} alt='' />
+                    </Col>
+
+
+
+                </Row>
+
+                < Footer />
+            </Container>
+        </section>
+
 
     )
 }
