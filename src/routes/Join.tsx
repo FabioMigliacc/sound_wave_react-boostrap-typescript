a3f4398d0e20235f3f870f3faec98e836b48865c
Fabio Migliaccio <febbio9@gmail.com>
2023-02-09 20:42:01 +0100

    join desktop version ultimate

--- src/routes/Join.tsx
+++ src/routes/Join.tsx
@@ -8,33 +8,65 @@ import styles from './join.module.css'
 export const Join = () => {
   return (<>
     <Menu />
-   <Container>
-    
-   <h1 className={styles.h1}>  Join   the <span className={styles.span}>fun.</span> </h1>
-    <Form className={styles.form}>
-      <Form.Group className="mb-4" controlId="formBasicEmail">
-        <Form.Label>Name</Form.Label>
-        <Form.Control type="text" className={styles.textinput} />
-        
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email"   className={styles.textinput} />
-        
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password"  className={styles.textinput} />
-      </Form.Group>
-      
-      <Button  variant="primary" type="submit" >
-        Submit
-      </Button>
-    </Form>
-    </Container>
-    <Footer />
+    <>
+     <Container  className="d-sm-flex align-items-center justify-content" style={{marginTop: '50px', minWidth: '400px',maxWidth:'800px'}}>
+     <h1 className={styles.h1}> Join the <span style={{color: '#bc3a80'}}>fun.</span></h1>
+     <Form className={styles.form}>
+     <Form.Group className="mb-3" controlId="formBasicText">
+         <Form.Label>Name:</Form.Label>
+         <Form.Control style={{background: '#202027', border: '1px solid #2F303A' }} type="text"/>
+       </Form.Group>
+       <Form.Group className="mb-3" controlId="formBasicEmail">
+         <Form.Label>Email:</Form.Label>
+         <Form.Control style={{background: '#202027', border: '1px solid #2F303A' }} type="email"/>      </Form.Group>
+       <Form.Group className="mb-3" controlId="formBasicPassword">
+         <Form.Label>Password:</Form.Label>
+        <Form.Control style={{background: '#202027', border: '1px solid #2F303A' }} type="password"/>
+       </Form.Group>
+       <Button variant="primary" type="submit" style={{width: '100%'}} >Join Now</Button>
+   </Form>
+    </Container>     <Footer/>
+     </>
     </>
 )
 }
+
+// import React from 'react'
+// import  Footer  from '../components/footer';
+// import Button from 'react-bootstrap/Button';
+// import Form from 'react-bootstrap/Form';
+// import {Container} from 'react-bootstrap';
+
+
+
+// type Props = {}
+
+
+// const Join = (props: Props) => {
+//   return (
+//     <>
+//     <Container  className="d-sm-flex align-items-center justify-content" style={{marginTop: '50px', minWidth: '400px',maxWidth:'800px'}}>
+//     <h1 style={{fontWeight: 'bold', fontSize: '4rem'}}>Join the <span style={{color: '#bc3a80'}}>fun.</span></h1>
+//     <Form style={{background: '#202027', width: '80%', borderRadius: '20px', padding: 20}}>
+//     <Form.Group className="mb-3" controlId="formBasicText">
+//         <Form.Label>Name:</Form.Label>
+//         <Form.Control style={{background: '#202027', border: '1px solid #2F303A' }} type="text"/>
+//       </Form.Group>
+//       <Form.Group className="mb-3" controlId="formBasicEmail">
+//         <Form.Label>Email:</Form.Label>
+//         <Form.Control style={{background: '#202027', border: '1px solid #2F303A' }} type="email"/>
+//       </Form.Group>
+//       <Form.Group className="mb-3" controlId="formBasicPassword">
+//         <Form.Label>Password:</Form.Label>
+//         <Form.Control style={{background: '#202027', border: '1px solid #2F303A' }} type="password"/>
+//       </Form.Group>
+//       <Button variant="primary" type="submit" style={{width: '100%'}} >Join Now</Button>
+//     </Form>
+//     </Container>
+//     <Footer/>
+//     </>
+//   )
+// }
+// export default Join;
+
 
